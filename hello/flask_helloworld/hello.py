f2571ce2eaa4034b60d45613861975fc8c36d01f
humancool <humancool@gmail.com>
2014-04-09 19:19:02 +0800

    adding new experiment

---
+++ hello/flask_helloworld/hello.py
@@ -0,0 +1,34 @@
+from flask import Flask, url_for
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['get', 'post'])
+def index():
+    if request.method == "post":
+        return "this is the post result"
+    return render_template("hello.html")
+
+
+@app.route("/hello/<username>")
+def hello(username):
+    return "Hello %s!" % username
+
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
+
+with app.test_request_context():
+    print url_for("index")
+
+if __name__ == "__main__":
+    app.run(debug=True)
